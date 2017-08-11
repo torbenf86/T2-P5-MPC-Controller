f0017306282f969bea03922c1b6cfb2c5a846a56
torbenf86 <torbenfischer@web.de>
2017-08-11 17:59:22 +0200

    Add files via upload

---
+++ MPC.h
@@ -0,0 +1,34 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+struct Result {
+
+		vector<double> x;
+		vector<double> y;
+		vector<double> delta;
+		vector<double> a;
+};
+
+class MPC {
+ public:
+  MPC();
+
+  double delta_old {0};
+  double a_old {0};
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  Result Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+
+
+};
+
+
+#endif /* MPC_H */
